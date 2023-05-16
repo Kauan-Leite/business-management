f0b54ffa108ae076fc03aecf6c5f15c87bb79a62
Kauan <kauan.s.leite@hotmail.com>
2023-05-16 12:15:56 -0300

    fix: Sequelize and Seeders

---
+++ backend/src/seeders/20230515224040-enterprise.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('enterprise', [
+      {
+        nome: 'Osten Moove',
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('enterprise', null, {})
+  }
+};
--- backend/src/services/empresa.service.js
+++ backend/src/services/empresa.service.js
@@ -1,7 +1,9 @@
-const { Empresa } = require('../models');
+const { Enterprise, Address } = require('../models');
 
 const getAll = async () => {
-  const empresas = await Empresa.findAll({});
+  const empresas = await Enterprise.findAll({
+    include: { model: Address, as: 'address', attributes: ['street', 'number', 'district', 'city', 'state'] },
+  });
 
   return empresas;
 };
